024a0e6ad195a008369d51e1b43d2ffb3593f359
Stephanie Glover <stephanie.glover@ufl.edu>
2019-03-18 12:00:14 -0700

    Add temp and show hidden messages

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // It is always helpful to use comments in your code!
+
+//Boiling Temp
+const span = document.getElementById('boiling-temp');
+const t = document.createTextNode("212");
+span.appendChild(t);
+
+//Freezing Temp
+const spanfreez = document.getElementById('freezing-temp');
+const w = document.createTextNode("32");
+spanfreez.appendChild(w);
+
+//Variable Temp
+const spanvar = document.getElementById('water-temp');
+let wt = document.createTextNode("68");
+spanvar.appendChild(wt);
+
+//Toggle Show & hide
